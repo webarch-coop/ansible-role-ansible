3713dd473b56e3346b2d898683bb8286fd35a461
Chris Croome <chris@webarchitects.co.uk>
2018-07-12 14:56:31 +0100

    added ./run.sh shortcut

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -z "${DISTRO}" ]]; then
+  echo 'Please: `export DISTRO="stretch"`'
+  exit 1
+fi
+
+if [[ -z "${SERVERNAME}" ]]; then
+  echo 'Please: `export SERVERNAME="example.org.uk"`'
+  exit 1
+fi
+
+echo "Running: ansible-playbook webarch.yml -i ${SERVERNAME}, -e \"hostname=${SERVERNAME} distro=${DISTRO}\" -v"
+
+ansible-playbook webarch.yml -i ${SERVERNAME}, -e "hostname=${SERVERNAME} distro=${DISTRO}" -v
+
